0bb82d27d041dd386a07286e6fb344a31be0880f
Amy Foosh <120824099+AmyFoosh@users.noreply.github.com>
2023-06-26 19:14:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// Get access to button.
+// Get access to HTML elements.
 let randomColorBtn = document.getElementById("random-color-btn");
 let container = document.getElementById("container");
 let hexText = document.getElementById("hex-text");
 let rgbText = document.getElementById("rgb-text");
 let body = document.querySelector("body");
 
+// Prevent right click on webpage.
 body.addEventListener("contextmenu", (event) => {
 
     event.preventDefault();
@@ -21,11 +22,11 @@ randomColorBtn.addEventListener("click", (event) => {
     // Create RGB color.
     let rgb = `rgb(${r}, ${g}, ${b})`;
 
+    // Change body and container BG color.
     body.style.backgroundColor = rgb;
     container.style.backgroundColor = "white";
 
+    // Create text display for HEX and RGB color.
     hexText.innerHTML = "#" + r.toString(16) + g.toString(16) + b.toString(16);
     rgbText.innerHTML = rgb;
-
-    console.log(rgb);
 });
